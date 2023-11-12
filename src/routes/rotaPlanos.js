65dbc38253b297cf365f5ec341fb0bffc3968b63
MatheusRCG <matheus.rcg.0@gmail.com>
2023-11-12 18:33:21 -0400

    arrumando getPlanosByID - arrumado codigo http no Swagger

--- src/routes/rotaPlanos.js
+++ src/routes/rotaPlanos.js
@@ -5,13 +5,10 @@ const mysql = require('../mysql').pool;
 
 //retornar todos os planos
 router.get('/', (req, res) => {
-  //res.json(planos);
-  
   mysql.getConnection((error, conn) => {
     if (error) {
       return res.status(500).send({error: error});
     }
-
     conn.query(
       'SELECT * FROM planos;',
       (error, resultado, field) => {
@@ -26,34 +23,43 @@ router.get('/', (req, res) => {
 
 //retornar plano por ID
 router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  res.json(buscarObjetoPorId(planos, id));
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({error: error});
+    }
+    conn.query(
+      'SELECT * FROM planos WHERE id_planos = ?;',
+      [req.params.id],
+      (error, resultado, field) => {
+        if (error) {
+          return res.status(500).send({error: error});
+        }
+        return res.status(200).send({response: resultado});
+      }
+    )
+  });
 });
 
 // cria um novo plano
 router.post('/', (req, res) => {
-  
   mysql.getConnection((error, conn) => {
-    
     if (error) {
-      return res.status(500).send({error: error});
+      return res.status(500).send({
+        error: error,
+        response: null
+      });
     }
-
     conn.query(
       'INSERT INTO planos (nome_plano, valor) VALUES (?,?)',
       [req.body.nome_plano, req.body.valor],
       (error, resultado, field) => {
         conn.release(); //fechando a conexão
-
         if(error) {
           return res.status(500).send ({
             error: error,
             response: null
           });
         }
-
-        console.log('Resultado da consulta:', resultado);
-
         res.status(201).send({
           mensagem: 'Plano criado com sucesso'
         });
@@ -61,14 +67,33 @@ router.post('/', (req, res) => {
     )
   });
 });
-
+/*
 // atualiza um plano existente
 router.put('/planos/:id', (req, res) => {
-  const id = req.params.id;
-  const novoPlano = req.body;
-  novoPlano.id = id;
-  planos[buscarIndiceDoObjetoPorId(planos, id)] = novoPlano;
-  res.send('Plano atualizado com sucesso');
+  mysql.getConnection((error, conn) => {
+    if (error) {
+      return res.status(500).send({
+        error: error,
+        response: null
+      });
+    }
+    conn.query(
+      'UPDATE produtos SET nome = ?, ',
+      [req.body.nome_plano, req.body.valor],
+      (error, resultado, field) => {
+        conn.release(); //fechando a conexão
+        if(error) {
+          return res.status(500).send ({
+            error: error,
+            response: null
+          });
+        }
+        res.status(201).send({
+          mensagem: 'Plano criado com sucesso'
+        });
+      }
+    )
+  });
 });
 
 // remove um plano existente
@@ -78,5 +103,6 @@ router.delete('/planos/:id', (req, res) => {
   res.send('Plano removido com sucesso');
 });
 
+*/
 
 module.exports = router;
